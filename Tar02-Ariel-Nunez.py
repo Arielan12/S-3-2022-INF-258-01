753562006f8f7067e3d23a27fbfee7635daace71
Arielan12 <113396672+Arielan12@users.noreply.github.com>
2022-09-16 19:32:33 -0400

    Add files via upload

---
+++ Tar02-Ariel-Nunez.py
@@ -0,0 +1,27 @@
+# A continuacion le mostrare como sacar la raiz cuadrada de dos numero y podra apreciar el proceso de como se hace.
+
+import math
+
+#Aqui ponemos el numero deseado para saber la raiz deseada
+print("Ingrese el primer numero: ")
+Numero1=int(input("De que numero desea saber la raiz:"))
+Numero2=int(input("De que numero desea saber la raiz:"))
+#Ya aqui despues de haber puesto las variables proseguimos a imprimir los resultados de las variables
+#Aqui elevamos nuestras variables al cuadrado y procedemos con el ejercicio.
+print("elevando al cuadrado:",Numero1,"=",Numero1**2)
+print("elevando al cuadrado:",Numero2,"=",Numero2**2)
+#Ya con los  resultados de las variables sumamos los dos resultados para tener los resultados finales
+SumaCuadrado = Numero1*2 + Numero2*2
+print("La suma es:", SumaCuadrado)
+print("Resultado final:",math.sqrt(SumaCuadrado))
+#Ya aqui finaliza el programa con todos los resultados correctos y acertables.
+
+
+
+
+
+
+
+
+
+
